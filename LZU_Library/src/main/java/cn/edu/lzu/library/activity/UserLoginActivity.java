633f7336a3d72290a0e0c2896de78b25c92585b5
16216 <liang.zhao02@hand-china.com>
2017-08-18 10:48:56 +0800

    use mvp to change login

--- LZU_Library/src/main/java/cn/edu/lzu/library/activity/UserLoginActivity.java
+++ LZU_Library/src/main/java/cn/edu/lzu/library/activity/UserLoginActivity.java
@@ -10,12 +10,10 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.TextUtils;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
@@ -27,28 +25,18 @@ import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.ScrollView;
 
-import com.google.gson.Gson;
-import com.zhy.http.okhttp.callback.StringCallback;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import cn.edu.lzu.library.R;
-import cn.edu.lzu.library.dao.user.User;
-import cn.edu.lzu.library.dao.user.UserCenter;
-import cn.edu.lzu.library.dao.user.UserDao;
-import cn.edu.lzu.library.dao.user.UserSQLiteOpenHelper;
-import cn.edu.lzu.library.protocol.UserCenterProtocol;
-import cn.edu.lzu.library.utils.UIUtils;
-import okhttp3.Call;
 
 /**
  * A login screen that offers login via userName/password.
- *   wtf  登录注册页面
+ * wtf  登录注册页面
  */
-public class UserLoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+public class UserLoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor>, IUserLoginView {
 
     /**
      * Id to identity READ_CONTACTS permission request.
@@ -86,13 +74,14 @@ public class UserLoginActivity extends AppCompatActivity implements LoaderCallba
 
     @BindView(R.id.login_form)
     ScrollView mLoginFormView;
+
+    UserLoginPresenter presenter;
+
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      * 跟踪登录任务，以确保我们可以根据需要取消登录。
      */
 //    private UserLoginTask mAuthTask = null;
-    private UserSQLiteOpenHelper mHelper = new UserSQLiteOpenHelper(this);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -100,23 +89,34 @@ public class UserLoginActivity extends AppCompatActivity implements LoaderCallba
         ButterKnife.bind(this);
         initView();
 
+        presenter = new UserLoginPresenter(this);
+
         // Set up the login form. 设置登录表单
         // populateAutoComplete();
 
+
         mPasswordView.setOnEditorActionListener((textView, id, keyEvent) -> {
             if (id == R.id.login || id == EditorInfo.IME_NULL) {
-                attemptLogin();
+
+                // Store values at the time of the login attempt.获取用户名和密码
+                actionAttemptLogin();
                 return true;
             }
             return false;
         });
 
-        mUserNameSignInButton.setOnClickListener(view -> attemptLogin());
+        mUserNameSignInButton.setOnClickListener(view -> actionAttemptLogin());
 
     }
 
+    private void actionAttemptLogin() {
+        String userName = mUserName.getText().toString().trim();
+        String password = mPasswordView.getText().toString().trim();
+        presenter.attemptLogin(userName, password);
+    }
+
     private void initView() {
-        tbLogin.setTitle(getString(R.string.user_login));
+        tbLogin.setTitle(R.string.user_login);
         setSupportActionBar(tbLogin);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
@@ -156,123 +156,10 @@ public class UserLoginActivity extends AppCompatActivity implements LoaderCallba
 
 
     /**
-     * Attempts to sign in or register the account specified by the login form.
-     * 尝试登录或注册登录表单指定的帐户。
-     * If there are form errors (invalid userName, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
-     * 如果存在表单错误（无效的用户名，缺少的字段等），则会显示错误，并且不会进行实际的登录尝试。
-     */
-    private void attemptLogin() {
-        mPasswordView.clearFocus();
-//        if (mAuthTask != null) {
-//            return;
-//        }
-
-        // Reset errors. 设置错误标志图标
-        mUserName.setError(null);
-        mPasswordView.setError(null);
-
-        // Store values at the time of the login attempt.获取用户名和密码
-        String userName = mUserName.getText().toString().trim();
-        String password = mPasswordView.getText().toString().trim();
-
-        boolean cancel = false;
-        View focusView = null;
-
-        // Check for a valid password, if the user entered one. 校验密码
-        if (TextUtils.isEmpty(password)) {
-            mPasswordView.setError(getString(R.string.error_field_required));
-            focusView = mPasswordView;
-            cancel = true;
-        } else if (!isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
-            focusView = mPasswordView;
-            cancel = true;
-        }
-
-        // Check for a valid userName. 校验用户名
-        if (TextUtils.isEmpty(userName)) {
-            mUserName.setError(getString(R.string.error_field_required));
-            focusView = mUserName;
-            cancel = true;
-        } else if (!isUserNameValid(userName)) {
-            mUserName.setError(getString(R.string.error_invalid_user_name));
-            focusView = mUserName;
-            cancel = true;
-        }
-
-        if (cancel) {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
-            focusView.requestFocus();
-        } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-            showProgress(true);
-            // : 2017/4/17 新建子线程并访问网络
-            actionLogin(userName,password);
-//            mAuthTask = new UserLoginTask(new User(userName, password));
-//            mAuthTask.execute((Void) null);
-        }
-    }
-
-    private void actionLogin(String userName, String password) {
-        User mUser = new User(userName,password);
-        new Thread(){
-            @Override
-            public void run() {
-                UserCenterProtocol.login(mUser, new StringCallback() {
-
-                    @Override
-                    public void onError(Call call, Exception e, int id) {
-                    }
-
-                    @Override
-                    public void onResponse(String response, int id) {
-
-//                        UIUtils.runOnUIThread(() -> showProgress(true));
-
-                        UserCenter userCenter =  new Gson().fromJson(response, UserCenter.class);
-                        int result = userCenter.result;
-                        if (result == 0) {
-                            UIUtils.runOnUIThread(() -> {
-                                // 验证失败
-                                showProgress(false);
-                                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                                mPasswordView.requestFocus();
-                            });
-
-                        } else {
-                            // 验证成功
-                            saveRecord(mUser, response);
-                            // 验证成功，开始给控件赋值
-                            // 将 userCenter 返回给上一个 activity
-                            Intent intent = getIntent().putExtra("loginResult", response);
-                            setResult(RESULT_OK, intent);
-                            finish();
-                        }
-                    }
-                });
-            }
-        }.start();
-    }
-
-    private boolean isUserNameValid(String userName) {
-        // 校验用户名的逻辑
-        String reg = "[0-9]{13}";
-        return userName.matches(reg);
-    }
-
-    private boolean isPasswordValid(String password) {
-        // 个人校验密码的逻辑
-        return password.length() >= 4;
-    }
-
-    /**
      * Shows the progress UI and hides the login form.
      * 显示进度条，隐藏表单
      */
-    private void showProgress(final boolean show) {
+    public void showProgress(final boolean show) {
         // 显示进度条
         int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
 
@@ -341,6 +228,57 @@ public class UserLoginActivity extends AppCompatActivity implements LoaderCallba
         mUserName.setAdapter(adapter);
     }
 
+    @Override
+    public void resetUI() {
+        mPasswordView.clearFocus();
+//        if (mAuthTask != null) {
+//            return;
+//        }
+
+        // Reset errors. 设置错误标志图标
+        mUserName.setError(null);
+        mPasswordView.setError(null);
+
+    }
+
+    @Override
+    public void passwordErr() {
+        mPasswordView.setError(getString(R.string.error_invalid_password));
+//        focusView = mPasswordView;
+    }
+
+    @Override
+    public void userNameErr() {
+        mUserName.setError(getString(R.string.error_invalid_user_name));
+    }
+
+    @Override
+    public void loginErr() {
+        // 验证失败
+        showProgress(false);
+        mPasswordView.setError(getString(R.string.error_incorrect_password));
+        mPasswordView.requestFocus();
+    }
+
+    @Override
+    public void loginSuccess(String response) {
+        // 验证成功，开始给控件赋值
+        // 将 userCenter 返回给上一个 activity
+        Intent intent = getIntent().putExtra("loginResult", response);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    @Override
+    public void passwordEmpty() {
+        mPasswordView.setError(getString(R.string.error_field_required));
+    }
+
+    @Override
+    public void userNameEmpty() {
+        mUserName.setError(getString(R.string.error_field_required));
+    }
+
 
     private interface ProfileQuery {
         String[] PROJECTION = {
@@ -352,16 +290,5 @@ public class UserLoginActivity extends AppCompatActivity implements LoaderCallba
         int IS_PRIMARY = 1;
     }
 
-
-
-    /**
-     * 保存本地记录，将用户和 json 结果保存起来
-     *
-     * @param user            用户
-     * @param loginResultJson 登录成功的 json 结果
-     */
-    private void saveRecord(User user, String loginResultJson) {
-        UserDao.saveUser(mHelper, user, loginResultJson);
-    }
 }
 
